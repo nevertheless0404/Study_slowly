a6b55915b2bc2e55fec7ce218fd7ab6cf36c9e57
nevertheless0404 <nevertheless.hi@gmail.com>
2022-08-11 21:15:00 +0900

    20220810

--- 2022.08.10/자릿수 더하기.py
+++ 2022.08.10/자릿수 더하기.py
@@ -1,5 +1,6 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QPRjqA10DFAUq
 
+# 자연수를 문자열 형태로 입력받아 각 자리수를 정수형으로 변환
 n = input()
 result = 0
 for i in range(len(n)):
